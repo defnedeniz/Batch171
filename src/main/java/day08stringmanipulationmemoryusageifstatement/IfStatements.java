bcd7fdc1bdb7bf9eb9b9df5f5d9980eed1f30587
defnedeniz <utopyadz@gmail.com>
2023-05-13 23:25:49 +0300

    vbersion1

---
+++ src/main/java/day08stringmanipulationmemoryusageifstatement/IfStatements.java
@@ -0,0 +1,34 @@
+package day08stringmanipulationmemoryusageifstatement;
+
+public class IfStatements {
+    public static void main(String[] args) {
+
+        //if statemets
+        //Bazi kodlari bazi sartlara gore aktive edebilmek icin if statemet kullanilir
+        //if eger demek ==>if cumlesi (hukum bıldiren cumle )
+
+        //if you study hard,you will learn java==>english
+       /*
+        if (you study hard){
+            you will learn java  ===>Java
+        }
+        */
+
+        //Ornek 1: sayi pozitif ise ekrana pozitif yazdırın
+
+        int num = 13;
+
+        if (num > 0) {
+            System.out.println(num + " ==> pozitif");
+        }
+
+        //Ornek2:sayi -1 ile 10 arasindaysa ekrana "Rakam " yazdırın
+
+        int n = 5;
+
+        if (n > -1 && n < 10) {
+            System.out.println("Rakam");
+        }
+
+    }
+}
