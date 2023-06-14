95806a110a4cbbf1fa9d3af454000cf2a4d8120e
defnedeniz <utopyadz@gmail.com>
2023-06-14 21:54:35 +0300

    v12

---
+++ src/main/java/day25inheritance/Car.java
@@ -0,0 +1,11 @@
+package day25inheritance;
+
+public class Car extends Vehicle{
+    public Car(){
+        this("make");
+        System.out.println("Car 1");
+    }
+    public Car(String make){
+        super();
+        System.out.println("Car 2");
+    }}
